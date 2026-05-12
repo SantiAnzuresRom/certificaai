6128218c5bececf24c0ddda234d13dc2b66cc551
SantiAnzuresRom <utsjrsantianzurom@gmail.com>
2026-05-12 07:36:26 -0600

    fix smtp render

--- src/routes/admin/create-user/createUser.ts
+++ src/routes/admin/create-user/createUser.ts
@@ -47,13 +47,20 @@ router.post("/", async (req, res) => {
       .set(initialProgress);
 
     // Configurar correo
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
     const mailOptions = {
       from: `"CertificaAI Support" <${process.env.SMTP_USER}>`,
